f587af53c01f3d902603ff65407b6dfe1080e160
vantandev <bvtan.dev@gmail.com>
2022-11-18 23:39:57 +0700

    Complete some page

--- src/App.js
+++ src/App.js
@@ -1,55 +1,8 @@
+import React from 'react';
+import Router from './router/Router';
 
-import './App.css'
-import Header from './components/header/Header'
-import { BrowserRouter as Router,Switch,Route} from "react-router-dom";
-import Pages from './pages/Pages';
-import Fldata from './components/flashdeal/Fldata';
-import { useState } from 'react';
-import Cart from './components/cart/Cart';
-import Shopdata from './components/shop/Shopdata';
-import Footer from './components/footer/Footer'
+const App = () => {
+  return <Router />;
+};
 
-function App() {
-
-  const {productItems} = Fldata
-  const {shopItems} = Shopdata
-
-  const [cartItem, setCardItem] = useState([])
-
-  const addToCart = (product) =>{
-    const productExit = cartItem.find((item) => item.id === product.id)
-
-    if(productExit){
-      setCardItem(cartItem.map((item) => (item.id === product.id ? {...productExit, qty:productExit.qty + 1} : item)))
-    }else {
-      setCardItem([...cartItem, {...product, qty: 1}])
-    }
-  }
-
-  const decreaseQty = (product) =>{
-    const productExit = cartItem.find((item) => item.id === product.id)
-    if (productExit.qty === 1 ){
-      setCardItem (cartItem.filter((item)=> item.id !== product.id))
-    }else {
-      setCardItem(cartItem.map((item) => (item.id === product.id? {...productExit, qty: productExit.qty - 1} : item)))
-    }
-  }
-  return (
-    <>
-      <Router>
-        <Header cartItem={cartItem}/>
-        <Switch>
-          <Route path='/' exact>
-            <Pages productItems={productItems} addToCart={addToCart} shopItems={shopItems}/>
-          </Route>
-          <Route path='/cart' exact>
-            <Cart cartItem={cartItem} addToCart={addToCart} decreaseQty={decreaseQty} />
-          </Route>
-        </Switch>
-        <Footer />
-    </Router>
-    </>
-  )
-}
-
-export default App
+export default App;
